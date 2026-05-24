9837a3f4a28edd70d9d44fda281eb2c68efa18fd
ayankhan2310 <117012843+ayankhan2310@users.noreply.github.com>
2026-05-24 20:17:11 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello World! this is second file");
-        int a = 10;
-        System.out.println("a " +a );
+        int aa = 10;
+        System.out.println("a " +aa );
+        
     }
 }
